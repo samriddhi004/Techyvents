2154cf9c62efd878eaac460f4785872d049f894e
kritisubedi011@gmail.com <kritisubedi011@gmail.com>
2024-10-24 15:48:15 +0545

    login signup

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,36 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+
+//login signup
+
+
+
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+
+// GET signup page
+router.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
+// POST login
+router.post('/login', function(req, res) {
+  const { username, password } = req.body;
+  // Implement your authentication logic here
+  // For example, check the username and password against a database
+  res.send(`Welcome back, ${username}!`);
+});
+
+// POST signup
+router.post('/signup', function(req, res) {
+  const { username, password } = req.body;
+  // Implement your sign-up logic here
+  // For example, save the user to a database
+  res.send(`User ${username} signed up successfully!`);
+});
+
+
 module.exports = router;
